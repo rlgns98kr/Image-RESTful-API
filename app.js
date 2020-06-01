6adf3694508b181937e93e13d8fe9d676c0ab278
GIHOON <rlgns98kr@gmail.com>
2020-06-01 23:21:38 +0900

    final commit

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-require("dotenv").config({ path: './.env' });
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const app = express();
 
-
 const fs = require("fs");
 const path = require("path");
-const mime = require("mime");
+const cors = require("cors");
 
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
@@ -13,19 +12,16 @@ app.use(express.urlencoded({ extended: false }));
 
 function checkfileexist(no) {
 	if (!fs.existsSync("./images")) fs.mkdirSync("./images");
-	return new Promise((resolve) => {
-		fs.readdir("./images", (error, file) => {
-			for (let value of file) {
-				if (path.basename(value, path.extname(value)) === no) resolve(value);
-			}
-			resolve(false);
-		});
-	});
+
+	let filenames = fs.readdirSync("./images");
+	for (let filename of filenames)
+		if (filename.replace(path.extname(filename), "") === no) return filename;
+	return false;
 }
 
-app.get("/images/:no", async (req, res) => {
+app.get("/images/:no", cors(), async (req, res) => {
 	const no = req.params.no;
-	if (no && /^[0-9]+$/.test(no)) {
+	if (/^[0-9]+$/.test(no)) {
 		const filename = await checkfileexist(no);
 		if (filename) {
 			res.status(200).sendFile(path.resolve("./images/" + filename));
@@ -34,62 +30,51 @@ app.get("/images/:no", async (req, res) => {
 				message: "The Image is not exist, input 'no': " + no,
 			});
 		}
-	} else if (no) {
+	} else {
 		res.status(400).json({
 			message: "'no'(body) must be Number, input 'no': " + no,
 		});
 	}
-	else {
-		res.status(400).json({
-			message: "'no'(body) is the essential value as Number!"
-		});
-	}
 });
 
-app.post("/images", async (req, res) => {
+app.post("/images", cors(), async (req, res) => {
 	const no = req.body.no;
 	const image = req.body.image;
-	if (no && /^[0-9]+$/.test(no)) {
+	if (/^[0-9]+$/.test(no)) {
 		const filename = await checkfileexist(no);
 		if (!filename) {
 			if (image) {
 				let file = req.body.image.match(/^data:([A-Za-z]+)\/([A-Za-z]+);base64,(.+)$/);
-				if (file && file.index === 0) {
-					if (file[1] === "image") {
-						if (file.length === 4 && (file[2] === "jpeg" || file[2] === "gif" || file[2] === "png")) {
-							let data = new Buffer.from(file[3], "base64");
-							let type = mime.getExtension(file[1] + '/' + file[2]);
-							try {
-								fs.writeFileSync(path.resolve("./images/" + no + '.' + type), data);
-								res.status(201).send();
-							} catch (e) {
-								res.status(409).json({
-									message: "File upload error! Please try later."
-								})
-							}
-						} else {
-							res.status(400).json({
-								message: "Invalid 'image'(body)! JPEG,PNG,GIF is only possible to upload"
-							})
-						}
-					} else {
-						res.status(400).json({
-							message: "'image'(body) is not image type!"
-						})
+				if (
+					file &&
+					file.index === 0 &&
+					file[1] === "image" &&
+					file.length === 4 &&
+					(file[2] === "jpeg" || file[2] === "gif" || file[2] === "png")
+				) {
+					let data = new Buffer.from(file[3], "base64");
+					try {
+						fs.writeFileSync(path.resolve("./images/" + no + "." + file[2]), data);
+						res.status(201).send();
+					} catch (e) {
+						res.status(409).json({
+							message: "File upload error! Please try later.",
+						});
 					}
 				} else {
 					res.status(400).json({
-						message: "'image'(body) is not valid on Base64"
-					})
+						message: "Invalid 'image'(body)! JPEG,PNG,GIF is only possible to upload",
+					});
 				}
 			} else {
 				res.status(400).json({
-					message: "'image'(body) is the essential value as Base64"
-				})
+					message: "Invalid 'image'(body)! JPEG,PNG,GIF is only possible to upload",
+				});
 			}
 		} else {
 			res.status(409).json({
-				message: "The Image is already exist, input 'no': " + no + ", You can use PUT Method!",
+				message:
+					"The Image is already exist, input 'no': " + no + ", You can use PUT Method!",
 			});
 		}
 	} else if (no) {
@@ -98,65 +83,55 @@ app.post("/images", async (req, res) => {
 		});
 	} else {
 		res.status(400).json({
-			message: "'no'(body) is the essential value as Number!"
+			message: "'no'(body) is the essential value as Number!",
 		});
 	}
 });
 
-app.put("/images/:no", async (req, res) => {
+app.put("/images/:no", cors(), async (req, res) => {
 	const no = req.params.no;
 	const image = req.body.image;
-	if (no && /^[0-9]+$/.test(no)) {
-		await checkfileexist(no);
+	if (/^[0-9]+$/.test(no)) {
+		const filename = await checkfileexist(no);
 		if (image) {
 			let file = req.body.image.match(/^data:([A-Za-z]+)\/([A-Za-z]+);base64,(.+)$/);
-			if (file && file.index === 0) {
-				if (file[1] === "image") {
-					if (file.length === 4 && (file[2] === "jpeg" || file[2] === "gif" || file[2] === "png")) {
-						let data = new Buffer.from(file[3], "base64");
-						let type = mime.getExtension(file[1] + '/' + file[2]);
-						try {
-							fs.writeFileSync(path.resolve("./images/" + no + '.' + type), data);
-							res.status(201).send();
-						} catch (e) {
-							res.status(409).json({
-								message: "File upload error! Please try later."
-							})
-						}
-					} else {
-						res.status(400).json({
-							message: "Invalid 'image'(body)! JPEG,PNG,GIF is only possible to upload"
-						})
-					}
-				} else {
-					res.status(400).json({
-						message: "'image'(body) is not image type!"
-					})
+			if (
+				file &&
+				file.index === 0 &&
+				file[1] === "image" &&
+				file.length === 4 &&
+				(file[2] === "jpeg" || file[2] === "gif" || file[2] === "png")
+			) {
+				let data = new Buffer.from(file[3], "base64");
+				try {
+					if (filename) fs.unlinkSync(path.resolve("./images/" + filename));
+					fs.writeFileSync(path.resolve("./images/" + no + "." + file[2]), data);
+					res.status(201).send();
+				} catch (e) {
+					res.status(409).json({
+						message: "File upload error! Please try later.",
+					});
 				}
 			} else {
 				res.status(400).json({
-					message: "'image'(body) is not valid on Base64"
-				})
+					message: "Invalid 'image'(body)! JPEG,PNG,GIF is only possible to upload",
+				});
 			}
 		} else {
 			res.status(400).json({
-				message: "'image'(body) is the essential value as Base64"
-			})
+				message: "Invalid 'image'(body)! JPEG,PNG,GIF is only possible to upload",
+			});
 		}
-	} else if (no) {
-		res.status(400).json({
-			message: "'no'(body) must be Number, input 'no': " + no,
-		});
 	} else {
 		res.status(400).json({
-			message: "'no'(body) is the essential value as Number!"
+			message: "'no'(body) must be Number, input 'no': " + no,
 		});
 	}
 });
 
-app.delete("/images/:no", async (req, res) => {
+app.delete("/images/:no", cors(), async (req, res) => {
 	const no = req.params.no;
-	if (no && /^[0-9]+$/.test(no)) {
+	if (/^[0-9]+$/.test(no)) {
 		const filename = await checkfileexist(no);
 		if (filename) {
 			fs.unlinkSync(path.resolve("./images", filename));
@@ -173,9 +148,9 @@ app.delete("/images/:no", async (req, res) => {
 	}
 });
 
-app.head("/images/:no", async (req, res) => {
+app.head("/images/:no", cors(), async (req, res) => {
 	const no = req.params.no;
-	if (no && /^[0-9]+$/.test(no)) {
+	if (/^[0-9]+$/.test(no)) {
 		const filename = await checkfileexist(no);
 		if (filename) {
 			res.status(200).sendFile("images/" + no);
@@ -193,10 +168,11 @@ app.head("/images/:no", async (req, res) => {
 
 app.options("/images/:no", async (req, res) => {
 	const no = req.params.no;
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	if (no && /^[0-9]+$/.test(no)) {
 		const filename = await checkfileexist(no);
 		if (filename) {
-			res.setHeader("Allow", "GET, PUT, DELETE, HEAD, OPTIONS");
+			cors({ methods: "GET, PUT, DELETE, HEAD, OPTIONS" });
 			res.status(204).send();
 		} else {
 			res.setHeader("Allow", "PUT, HEAD, OPTIONS");
@@ -208,8 +184,25 @@ app.options("/images/:no", async (req, res) => {
 	}
 });
 app.options("/images", (req, res) => {
-	res.setHeader("Allow", "POST, OPTIONS");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Allow", "HEAD, POST, OPTIONS");
 	res.status(204).send();
+});
+
+app.get("/images", cors(), (req, res) => {
+	res.status(400).json({
+		message: "'no'(body) is the essential value as Number!",
+	});
+});
+app.put("/images", cors(), (req, res) => {
+	res.status(400).json({
+		message: "'no'(body) is the essential value as Number!",
+	});
+});
+app.delete("/images", cors(), (req, res) => {
+	res.status(400).json({
+		message: "'no'(body) is the essential value as Number!",
+	});
 });
 
 app.listen(process.env.PORT, () => {
